341ccb1ede54901f68843df64346f16a347bac67
Ashley Almonte <ashleyalmonte14@gmail.com>
2019-05-01 11:27:37 -0400

    first commit

---
+++ main.js
@@ -0,0 +1,31 @@
+  // SCOPES
+
+  // let
+  // var
+  // const
+  // no keyword -- GLobal variable
+
+  let str = "Welcome to class"
+
+  function useless() {
+    let str = 'be a useless function'
+    console.log(str)
+
+  }
+    function speak() {
+    console.log(str);
+    if (condition == true) {
+   }
+
+  function calcTaxNY(Price) {
+    let tax = 1000000
+    console.log(price + tax)
+  }
+  function calTaxXs(price) {
+    let tax = 3
+    console.log(price + tax)
+  }
+
+  calctaxNY()
+  useless()
+  speak()
