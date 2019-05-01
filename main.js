e9e666a5d26aa3ba3f2cf05edeaeed9c4d6eb740
Ashley Almonte <ashleyalmonte14@gmail.com>
2019-05-01 12:54:48 -0400

    added defintions closure/name spacing

--- main.js
+++ main.js
@@ -1,15 +1,5 @@
-  // NAMESPACE
+  //
+Namespace- when you have the same name but different function
+closures- the scope that a function or block has acess to
 
-  user = {
-    login: () => {
-      console.log('under has logged in')
-    }
-
-
-
-function- Block code that is run when its name is called
-scope- the lifespan of an object definition
-parometers/argument- value passed to functions
-ananymous  function- function w/o name
-return value- the value that return when a function is complete verb
-innovation- calling something
+block is something inside a curly bracket
